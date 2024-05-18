4de02e4c575b3c58e038fef4eb23c593a7b5f8fa
Augusyyy <8851@holbertonstudents.com>
2024-05-18 23:29:22 +1000

    goodone

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,47 +1,23 @@
-"""
-10-square.py
-题目要求：
-1 在9的基础上，添加类class Square，继承自 Rectangle
-2 class Square 中增加 def __init__(self, size):
-    检查size的合法性
-    并且赋值给class Square的成员变量self.__size
-    并且调用父亲的__init__
-        super.__inti__(self.__size,self.__size)
+#!/usr/bin/python3
+"""Square inherits from Rectangle which inherits from BaseGeometry
 """
 
 
-class BaseGeometry:
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-
-    def __init__(self, width, height):
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Square data that inherits from Rectangle
+       init `size` twice because width and height are same in squares
+    """
 
     def __init__(self, size):
-        self.integer_validator("size", size)
+        super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Method to calculate area of the square
+        """
+
+        return self.__size * self.__size
