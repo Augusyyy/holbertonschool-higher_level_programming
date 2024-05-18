37451ae7555cec10c6ab6ddce2dacd088e9d9959
Augusyyy <8851@holbertonstudents.com>
2024-05-18 23:27:50 +1000

    newone

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -10,6 +10,6 @@ def is_same_class(obj, a_class):
     Returns True if the object is exactly an instance
     of the specified class ; otherwise False.
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,6 +11,6 @@ def inherits_from(obj, a_class):
     class that inherited (directly or indirectly)
     from the specified class ; otherwise False.
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
