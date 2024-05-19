7c30ea9c56b86074839495984ec6edc1703ae1ef
Augusyyy <8851@holbertonstudents.com>
2024-05-19 11:49:47 +1000

    newfixed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -14,15 +14,10 @@ def say_my_name(first_name, last_name=""):
         first_name (str): the first name.
         last_name (str): the last name.
     """
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    if first_name and last_name:
-        print("My name is {:s} {:s}".format(first_name, last_name))
-    elif first_name:
-        print("My name is {:s}".format(first_name))
-    elif last_name:
-        print("My name is  {:s}".format(last_name))
-    else:
-        print("My name is")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
