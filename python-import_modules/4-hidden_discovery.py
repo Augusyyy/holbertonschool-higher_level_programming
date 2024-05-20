3b3f872202b3a0c65042193938c1ae91edc21179
Augusyyy <8851@holbertonstudents.com>
2024-05-20 10:50:54 +1000

    new4working

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
+import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+
+if __name__ == "__main__":
+
+    module_names = sorted(name for name in dir(hidden_4) if not name.startswith('__'))
+
+    for name in module_names:
+        print(name)
