94a2b00d65c7868471db7ef43aa7a02299e02463
Augusyyy <8851@holbertonstudents.com>
2024-05-25 23:36:02 +1000

    newoenpush

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -30,8 +30,8 @@ class Circle(Shape):
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        self.width = abs(width)
-        self.height = abs(height)
+        self.width = width
+        self.height = height
 
     def area(self):
         return self.width * self.height
