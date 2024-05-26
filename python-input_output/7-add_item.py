aed3a8729879849bb1b87325f66099214a79f98d
Augusyyy <8851@holbertonstudents.com>
2024-05-27 00:28:14 +1000

    new7up

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module for saving argv info via json to file."""
+'''Module for saving argv info via json to file.'''
 import json
 import os.path
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 file = "add_item.json"
 json_list = []
