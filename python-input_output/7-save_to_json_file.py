58b111a9956da6da268d837193542cbcc8c90b7e
Augusyyy <8851@holbertonstudents.com>
2024-05-27 00:20:27 +1000

    newonep

--- python-input_output/7-save_to_json_file.py
+++ python-input_output/7-save_to_json_file.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""Module for saving argv info via json to file."""
+"""Module for save_to_json_file method."""
 import json
-import os.path
-import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
 
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
-
-save_to_json_file(json_list, file)
+def save_to_json_file(my_obj, filename):
+    """Method for saving json file."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
