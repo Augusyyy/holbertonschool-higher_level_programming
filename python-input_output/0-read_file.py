48d0fc3abe0b998013f2b0b7bff6f33fe0f5f0b9
Augusyyy <8851@holbertonstudents.com>
2024-05-27 00:22:29 +1000

    newone

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, "r", encoding="utf-8") as file_obj:
-        print(file_obj.read())
+        str = file_obj.read()
+        print(str.strip())
