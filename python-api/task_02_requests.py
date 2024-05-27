d492ace65a5cb4f8cec46a8a8de8f53f2ff73465
Augusyyy <8851@holbertonstudents.com>
2024-05-28 01:16:35 +1000

    newupdate

---
+++ python-api/task_02_requests.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Write a basc Python menthod to fetch posts from
+https://jsonplaceholder.typicode.com/posts using requests.get().
+"""
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """
+    Print the status code of the response i.e. Status Code: 200
+    Iterate through the parsed JSON data and print out the titles
+    of all the posts.
+    """
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    print("Status Code: ", response.status_code)
+    if response.status_code == 200:
+        list_data = response.json()
+        for post in list_data:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    print("Status Code: ", response.status_code)
+    if response.status_code == 200:
+        with open("posts.csv", "w") as f:
+            titles = ['id', 'title', 'body']
+            writer = csv.DictWriter(f, fieldnames=titles)
+            list_data = response.json()
+            writer.writeheader()
+            for post in list_data:
+                writer.writerow({'id':post['id'], 'title': post['title'], 'body': post['body']})
+
+
+
+
+fetch_and_print_posts()
+fetch_and_save_posts()
---
+++ python-api/task_04_flask.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+""" a simple flask app """
+from flask import Flask, request
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """
+    get
+    home function return string
+    content-type is text
+    """
+    return "Welcome to the Flask API!"
+
+
+@app.route('/data')
+def get_data():
+    """
+    get
+    return dict’s key
+    content-type is application/json
+    """
+    users = {
+        "jane": {"name": "Jane", "age": 28, "city": "Los Angels"},
+        "john": {"name": "John", "age": 30, "city": "New York"}
+    }
+    keys = []
+    for key in users.keys():
+        keys.append(key)
+    return jsonify(keys)
+
+
+@app.route('/status')
+def get_status():
+    """
+    get
+    return ok
+    content-type is text
+    """
+    return "OK"
+
+
+@app.route('/users/<username>')
+def get_username(username):
+    """
+    get
+    return user info
+    content-type is application/json
+    """
+    users = {
+        "jane": {"name": "Jane", "age": 28, "city": "Los Angels"},
+        "john": {"name": "John", "age": 30, "city": "New York"}
+    }
+    if username in users:
+        return jsonify(users[username])
+        # return json.dumps(users[username]), 200, {"Content-Type": "application/json"}
+    else:
+        return "User does not exist"
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    users = {
+        "jane": {"name": "Jane", "age": 28, "city": "Los Angels"},
+        "john": {"name": "John", "age": 30, "city": "New York"}
+    }
+    data = request.get_json()
+    users[data["username"]] = {
+                'username': data["username"],
+                'name': data['name'],
+                'age': data['age'],
+                'city': data['city']
+            }
+    return {
+        'message': 'User added',
+        'user': {
+                'username': data["username"],
+                'name': data['name'],
+                'age': data['age'],
+                'city': data['city']
+            }
+        }
+
+if __name__ == '__main__':
+    app.run()
