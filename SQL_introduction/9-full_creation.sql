b6d5cc31811cd2342070e2f52fd964e6c0cc0dae
Augusyyy <8851@holbertonstudents.com>
2024-06-17 23:32:49 +1000

    fix 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,12 @@
 -- Creates and fills a table second_table with attributes id, name and score.
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
 INSERT INTO second_table (id, name, score) VALUES
-                                               (1, 'John', 10),
-                                               (2, 'Alex', 3),
-                                               (3, 'Bob', 14),
-                                               (4, 'George', 8);
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
