0e007b23beda4115a137f1090dc5dabab60d8fbe
Augusyyy <8851@holbertonstudents.com>
2024-06-18 10:35:36 +1000

    the fixed

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all genres from the database hbtn_0d_tvshows along with the number of
-SELECT g.name AS genre, COUNT(sg.show_id) AS number_of_shows
+SELECT g.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres AS g
-JOIN tv_show_genres AS sg
-ON g.id = sg.genre_id
+INNER JOIN tv_show_genres AS t
+ON g.id = t.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC;
--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,2 @@
 -- Creates the table unique_id.
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
