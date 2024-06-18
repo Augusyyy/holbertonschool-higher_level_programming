a0ef622916d41bfbdba7d8fb69464cd400305e8b
Augusyyy <8851@holbertonstudents.com>
2024-06-18 11:02:55 +1000

    the 1 2 fixed

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- Creates the table force_name.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
