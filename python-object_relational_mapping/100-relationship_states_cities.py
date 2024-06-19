2cc23466535455ad3b4dc5d9e9adac81551d5cef
Augusyyy <8851@holbertonstudents.com>
2024-06-19 19:09:03 +1000

    create_all

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,6 +11,7 @@ from relationship_city import Base, City
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -10,7 +10,7 @@ class State(Base):
     """
     Represents a state for a MySQL database.
     """
-    __tablename__ = "state"
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
