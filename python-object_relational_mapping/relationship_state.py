418e9362e8d37fd29d59afd99653d547bd92500d
Augusyyy <8851@holbertonstudents.com>
2024-06-19 19:02:11 +1000

    state

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -10,7 +10,7 @@ class State(Base):
     """
     Represents a state for a MySQL database.
     """
-    __tablename__ = "states"
+    __tablename__ = "state"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
