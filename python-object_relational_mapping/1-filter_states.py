19a0511d1671e579bd3f2b3577e1d61dc0580d71
Augusyyy <8851@holbertonstudents.com>
2024-06-19 18:52:15 +1000

    the BINARY

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute("SELECT `id`, `name` FROM `states`\
-     WHERE `name` LIKE 'N%' ORDER BY `id` ASC")
+     WHERE `name` LIKE BINARY 'N%' ORDER BY `id` ASC")
     rows = cursor.fetchall()
 
     for row in rows:
