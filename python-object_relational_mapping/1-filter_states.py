0e37bf8e2452762cae71d368d507a504b1be4921
Augusyyy <8851@holbertonstudents.com>
2024-06-19 18:50:06 +1000

    fixed form

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     """
     init database
     """
-    db = MySQLdb.connect(host="localhost", port=3306,user=argv[1], 
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
