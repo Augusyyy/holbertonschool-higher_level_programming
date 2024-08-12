6e4b3cbc7845a7120c156c09265046fff0f23b96
Augusyyy <8851@holbertonstudents.com>
2024-08-13 00:38:15 +1000

    all in

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,4 +1,10 @@
+101 
+
 #!/usr/bin/python3
+# 101-locked_class.py
+"""Defines a locked class."""
+
+
 class LockedClass:
     """
     Prevent the user from instantiating new LockedClass attributes
